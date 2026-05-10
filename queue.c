6c57cf4749d66844b952b2c1eb7e29d914a98366
Membau <baodatf@gmail.com>
2026-05-11 06:57:22 +0700

    Add files via upload
    fix 2.1 update 2.2.5

---
+++ queue.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "queue.h"
+
+int empty(struct queue_t *q)
+{
+        if (q == NULL)
+                return 1;
+        return (q->size == 0);
+}
+
+void enqueue(struct queue_t *q, struct pcb_t *proc)
+{
+        /* TODO: put a new process to queue [q] */
+        if (q == NULL || proc == NULL)
+                return;
+        if (q->size >= MAX_QUEUE_SIZE) {
+                return;
+        }
+        q->proc[q->size] = proc;
+        q->size++;
+}
+
+struct pcb_t *dequeue(struct queue_t *q)
+{
+        /* TODO: return a pcb whose prioprity is the highest
+         * in the queue [q] and remember to remove it from q
+         * */
+        if (q == NULL || q->size == 0)
+                return NULL;
+        struct pcb_t *temp = q->proc[0];
+        for (int i = 0; i<q->size-1; i++)
+        {
+                q->proc[i] = q->proc[i+1];  
+        }
+        q->proc[q->size-1] = NULL;
+        q->size--;
+        return temp;
+}
+
+struct pcb_t *purgequeue(struct queue_t *q, struct pcb_t *proc)
+{
+        /* TODO: remove a specific item from queue
+         * */
+        if (q == NULL || proc == NULL || empty(q)) return NULL;
+        for (int i= 0; i < q->size; i++)
+        {
+                if (q->proc[i] == proc )
+                {
+                        struct  pcb_t *temp = q->proc[i];
+                        
+                        if (i == q->size - 1) {
+                                q->proc[q->size] = NULL;
+                                q->size--;
+                                return temp;
+                        }
+                        for (int j=i; j < q->size-1; j++)
+                        {
+                                q->proc[j] = q->proc[j+1];
+                        }
+                        q->proc[q->size-1] = NULL;
+                        q->size--;
+                        return temp;
+                }
+        }
+        return NULL;
+}
